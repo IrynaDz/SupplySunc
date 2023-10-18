dbca4a980b99a8601f675220f6362bc64ff893fd
Altynai Iman <altysha5@Altynais-MacBook-Pro-2.local>
2023-10-18 15:12:49 -0500

    masters

---
+++ src/test/java/pages/MastersPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class MastersPage {
+    public MastersPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "input")
+    public WebElement login;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement signInButton;
+
+    @FindBy(xpath = "//span[text()='Masters']")
+    public WebElement mastersOption;
+
+    @FindBy(xpath = "//img[@alt='MenuIcon']")
+    public WebElement threeDots;
+
+    @FindBy(xpath = "//li[@role='menuitem' and text()='Delete']")
+    public WebElement deleteFromDropdown;
+
+    @FindBy(xpath = "//button[@type='button' and text()='Delete']")
+    public WebElement deleteRedButton;
+
+    @FindBy(xpath = "//button[@type='button' and text()='Cancer']")
+    public WebElement cancelButton;
+
+}
